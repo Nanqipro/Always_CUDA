0644b8f63d5b1544b2b2a38aef47161a2026f7cb
Nanqipro <2280285631@qq.com>
2024-08-11 17:20:36 +0800

    2024/8/11

---
+++ matrix_mul/naive.cu
@@ -0,0 +1,73 @@
+#include "error.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda_runtime.h>  // 包含CUDA头文件
+#ifdef USE_DP
+    typedef double real;
+#else
+    typedef float real;
+#endif
+
+#define CEIL_DIV(x, y) ((x + y - 1) / y)  // 定义CEIL_DIV宏
+
+// 计算矩阵乘法：C = A * B，矩阵A的维度为M*K，矩阵B的维度为K*N
+#define M 512
+#define K 512
+#define N 512
+
+// const int NUM_REPEATS = 100; //重复计算时间的次数
+
+// 初始化矩阵函数
+void initial(real *array, int size){
+    for(int i = 0; i < size; i++){
+        array[i] = (real)(rand() % 10 + 1);
+    }
+}
+
+
+__global__ void sgemm_naive(int M, int N, int K, float alpha, real *A, real *B, float beta, real *C){
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+    if(x < M && y < N){
+        real Cvalue = 0;
+        for(int i = 0; i < K; i++){
+            Cvalue += A[x * K + i] * B[i * N + y];
+            
+        }
+        // C = α*(A@B)+β*C
+        C[x*N + y] = alpha * Cvalue + beta * C[x*N + y];
+       
+    }
+}
+
+int main(){
+    int size_A = M * K;
+    int size_B = K * N;
+    int size_C = M * N;
+    real *A, *B, *C;
+    A = (real*)malloc(size_A * sizeof(real));
+    B = (real*)malloc(size_B * sizeof(real));
+    C = (real*)malloc(size_C * sizeof(real));
+    initial(A, size_A);
+    initial(B, size_B);
+
+    real *d_A, *d_B, *d_C;
+    cudaMalloc((void**)&d_A, size_A * sizeof(real));  
+    cudaMalloc((void**)&d_B, size_B * sizeof(real));
+    cudaMalloc((void**)&d_C, size_C * sizeof(real));
+
+    cudaMemcpy(d_A, A, size_A * sizeof(real), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, B, size_B * sizeof(real), cudaMemcpyHostToDevice);
+
+    dim3 gridDim(CEIL_DIV(M, 32), CEIL_DIV(N, 32),1);
+    dim3 blockDim(32, 32,1);
+    sgemm_naive<<<gridDim, blockDim>>>(M, N, K, 1.0f, d_A, d_B, 0.0f, d_C);
+    cudaMemcpy(C, d_C, size_C * sizeof(real), cudaMemcpyDeviceToHost);
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+    free(A);
+    free(B);
+    free(C);
+    return 0;
+}
