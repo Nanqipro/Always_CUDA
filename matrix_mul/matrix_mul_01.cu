d61588541af3f2ef2a852290085a09bc6713b1e1
Nanqipro <2280285631@qq.com>
2024-08-12 09:18:30 +0800

    2024/8/12

--- matrix_mul/matrix_mul_01.cu
+++ matrix_mul/matrix_mul_01.cu
@@ -23,7 +23,7 @@ void initial(real *array, int size){
 }
 
 
-void print_matrix(real *C, int M,int N){
+void print_matrix(real *C, int m,int n){
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < N; j++) {
             printf("%f ", C[i * N + j]);
@@ -45,7 +45,7 @@ __global__ void matrix_mul_01(real *A, real *B,real *C,int m, int k, int n){
 }
 
 
-void timing(real *d_A, real *d_B, real *d_C, int M, int K, int N){
+void timing(real *d_A, real *d_B, real *d_C, int m, int k, int n){
     int dimx = 2;
     int dimy = 2;
 
@@ -87,8 +87,8 @@ int main(int argc, char **argv){
     initial(A, size_A);
     initial(B, size_B);
 
-    print_matrix(A,M,K);
-    print_matrix(B,K,N);
+    // print_matrix(A,M,K);
+    // print_matrix(B,K,N);
     
     real *d_A, *d_B, *d_C;
     cudaMalloc((void**)&d_A, size_A * sizeof(real));  
@@ -118,3 +118,116 @@ int main(int argc, char **argv){
     free(C);
     return 0;
 }
+
+// #include <stdio.h>
+// #include "error.cuh"
+// #include <cuda_runtime.h>
+
+// // 矩阵乘法的朴素实现
+// // 计算矩阵乘法：C = A * B，矩阵A的维度为M*K，矩阵B的维度为K*N
+
+// #define M 512
+// #define K 512
+// #define N 512
+
+// const int NUM_REPEATS = 10;
+
+// void initial(float *array, int size){
+//     for(int i = 0; i < size; i++){
+//         array[i] = (float)(rand() % 10 + 1);
+//     }
+// }
+
+// void print_matrix(float *C,int m,int n){
+//     for (int i = 0; i < M; i++) {
+//         for (int j = 0; j < N; j++) {
+//             printf("%f ", C[i * N + j]);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// __global__ void matrix_mul_01(float *A, float *B, float *C, int m, int k, int n){
+//     int ix = blockIdx.x * blockDim.x + threadIdx.x;
+//     int iy = blockIdx.y * blockDim.y + threadIdx.y;
+//     if(ix < m && iy < n){
+//         float Cvalue = 0;
+//         for(int kk = 0; kk < k; kk++){
+//             Cvalue += A[ix * k + kk] * B[kk * n + iy]; //行优先矩阵
+//         }
+//         C[ix * n + iy] = Cvalue;
+//     }
+// }
+
+// void timing(float *d_A, float *d_B, float *d_C, int m, int k, int n){
+//     int dimx = 2;
+//     int dimy = 2;
+
+//     dim3 dimBlock(dimx,dimy);
+//     dim3 dimGrid((M+dimBlock.x-1)/dimBlock.x,(N+dimBlock.y-1)/dimBlock.y);
+
+//     float t1_sum = 0.0;
+//     for(int i = 0; i < NUM_REPEATS; i++){
+//         cudaEvent_t start, stop;
+//         CHECK(cudaEventCreate(&start));
+//         CHECK(cudaEventCreate(&stop));
+//         CHECK(cudaEventRecord(start, 0));
+//         cudaEventQuery(start);
+//         matrix_mul_01<<<dimGrid,dimBlock>>>(d_A,d_B,d_C,M,K,N);
+//         CHECK(cudaEventRecord(stop, 0));
+//         CHECK(cudaEventSynchronize(stop));
+//         float t1;
+//         CHECK(cudaEventElapsedTime(&t1, start, stop));
+//         printf("%f\n",t1);
+//         if(i>0){
+//             t1_sum += t1;
+//         }
+//         CHECK(cudaEventDestroy(start));
+//         CHECK(cudaEventDestroy(stop));
+//     }
+//     printf("Average time: %f\n",t1_sum/(NUM_REPEATS-1));
+// }
+
+// int main(int argc, char **argv){
+//     int size_A = M * K;
+//     int size_B = K * N;
+//     int size_C = M * N;
+//     float *A, *B, *C;
+//     A = (float*)malloc(size_A * sizeof(float));
+//     B = (float*)malloc(size_B * sizeof(float));
+//     C = (float*)malloc(size_C * sizeof(float));
+
+//     initial(A, size_A);
+//     initial(B, size_B);
+
+//     print_matrix(A,M,K);
+//     print_matrix(B,K,N);
+    
+//     float *d_A, *d_B, *d_C;
+//     cudaMalloc((void**)&d_A, size_A * sizeof(float));  
+//     cudaMalloc((void**)&d_B, size_B * sizeof(float));
+//     cudaMalloc((void**)&d_C, size_C * sizeof(float));
+
+//     cudaMemcpy(d_A, A, size_A * sizeof(float), cudaMemcpyHostToDevice);
+//     cudaMemcpy(d_B, B, size_B * sizeof(float), cudaMemcpyHostToDevice);
+
+//     int dimx = 2;
+//     int dimy = 2;
+
+//     dim3 dimBlock(dimx,dimy);
+//     dim3 dimGrid((M+dimBlock.x-1)/dimBlock.x,(N+dimBlock.y-1)/dimBlock.y);
+//     matrix_mul_01<<<dimGrid,dimBlock>>>(d_A,d_B,d_C,M,K,N);
+
+//     print_matrix(C,M,N);
+
+//     timing(d_A, d_B, d_C, M, K, N);
+
+//     cudaMemcpy(C, d_C, size_C * sizeof(float), cudaMemcpyDeviceToHost);
+//     cudaFree(d_A);
+//     cudaFree(d_B);
+//     cudaFree(d_C);
+//     free(A);
+//     free(B);
+//     free(C);
+//     return 0;
+// }
--- matrix_mul/naive.cu
+++ matrix_mul/naive.cu
@@ -1,33 +1,40 @@
-// #include "error.h"
+#include "error.cuh"
 #include <stdio.h>
 #include <stdlib.h>
 #include <cuda_runtime.h>  // 包含CUDA头文件
-// #define CEIL_DIV(x, y) ((x + y - 1) / y)  // 定义CEIL_DIV宏
 
-inline int CEIL_DIV(int x, int y) {
-    return (x + y - 1) / y;
-}
+#ifdef USE_DP
+    typedef double real;
+#else
+    typedef float real;
+#endif
+
+#define CEIL_DIV(x, y) ((x + y - 1) / y)  // 定义CEIL_DIV宏
+
+// inline int CEIL_DIV(int x, int y) {
+//     return (x + y - 1) / y;
+// }
 
 // 计算矩阵乘法：C = A * B，矩阵A的维度为M*K，矩阵B的维度为K*N
 #define M 512
 #define K 512
 #define N 512
 
-// const int NUM_REPEATS = 100; //重复计算时间的次数
+const int NUM_REPEATS = 100; //重复计算时间的次数
 
 // 初始化矩阵函数
-void initial(float *array, int size){
+void initial(real *array, int size){
     for(int i = 0; i < size; i++){
-        array[i] = (float)(rand() % 10 + 1);
+        array[i] = (real)(rand() % 10 + 1);
     }
 }
 
 
-__global__ void sgemm_naive(int M, int N, int K, float alpha, float *A, float *B, float beta, float *C){
+__global__ void sgemm_naive(float alpha, real *A, real *B, float beta, real *C){
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
     if(x < M && y < N){
-        float Cvalue = 0;
+        real Cvalue = 0;
         for(int i = 0; i < K; i++){
             Cvalue += A[x * K + i] * B[i * N + y];
             
@@ -38,29 +45,59 @@ __global__ void sgemm_naive(int M, int N, int K, float alpha, float *A, float *B
     }
 }
 
+void timing(real *d_A, real *d_B, real *d_C){
+    
+    dim3 gridDim(CEIL_DIV(M, 32), CEIL_DIV(N, 32),1);
+    dim3 blockDim(32, 32,1);
+
+    float t1_sum = 0.0;
+    for(int i = 0; i < NUM_REPEATS; i++){
+        cudaEvent_t start, stop;
+        CHECK(cudaEventCreate(&start));
+        CHECK(cudaEventCreate(&stop));
+        CHECK(cudaEventRecord(start, 0));
+        cudaEventQuery(start);
+        sgemm_naive<<<gridDim, blockDim>>>(1.0f, d_A, d_B, 0.0f, d_C);
+        CHECK(cudaEventRecord(stop, 0));
+        CHECK(cudaEventSynchronize(stop));
+        float t1;
+        CHECK(cudaEventElapsedTime(&t1, start, stop));
+        printf("%f\n",t1);
+        if(i>0){
+            t1_sum += t1;
+        }
+        CHECK(cudaEventDestroy(start));
+        CHECK(cudaEventDestroy(stop));
+
+    }
+    printf("Average time: %f\n",t1_sum/(NUM_REPEATS-1));
+}
+
 int main(){
     int size_A = M * K;
     int size_B = K * N;
     int size_C = M * N;
     float *A, *B, *C;
-    A = (float*)malloc(size_A * sizeof(float));
-    B = (float*)malloc(size_B * sizeof(float));
-    C = (float*)malloc(size_C * sizeof(float));
+    A = (real*)malloc(size_A * sizeof(real));
+    B = (real*)malloc(size_B * sizeof(real));
+    C = (real*)malloc(size_C * sizeof(real));
     initial(A, size_A);
     initial(B, size_B);
 
-    float *d_A, *d_B, *d_C;
-    cudaMalloc((void**)&d_A, size_A * sizeof(float));  
-    cudaMalloc((void**)&d_B, size_B * sizeof(float));
-    cudaMalloc((void**)&d_C, size_C * sizeof(float));
+    real *d_A, *d_B, *d_C;
+    cudaMalloc((void**)&d_A, size_A * sizeof(real));  
+    cudaMalloc((void**)&d_B, size_B * sizeof(real));
+    cudaMalloc((void**)&d_C, size_C * sizeof(real));
 
-    cudaMemcpy(d_A, A, size_A * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, B, size_B * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_A, A, size_A * sizeof(real), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, B, size_B * sizeof(real), cudaMemcpyHostToDevice);
 
-    dim3 gridDim(CEIL_DIV(M, 32), CEIL_DIV(N, 32),1);
-    dim3 blockDim(32, 32,1);
-    sgemm_naive<<<gridDim, blockDim>>>(M, N, K, 1.0f, d_A, d_B, 0.0f, d_C);
-    cudaMemcpy(C, d_C, size_C * sizeof(float), cudaMemcpyDeviceToHost);
+    // dim3 gridDim(CEIL_DIV(M, 32), CEIL_DIV(N, 32),1);
+    // dim3 blockDim(32, 32,1);
+    // sgemm_naive<<<gridDim, blockDim>>>(1.0f, d_A, d_B, 0.0f, d_C);
+    timing(d_A,d_B,d_C);
+
+    cudaMemcpy(C, d_C, size_C * sizeof(real), cudaMemcpyDeviceToHost);
     cudaFree(d_A);
     cudaFree(d_B);
     cudaFree(d_C);
