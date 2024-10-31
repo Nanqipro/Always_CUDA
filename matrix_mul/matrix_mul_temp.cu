5e898957402bafe61de2a5f007500919fa07abaa
Nanqipro <2280285631@qq.com>
2024-10-31 13:56:35 +0800

    20241031

--- matrix_mul/matrix_mul_temp.cu
+++ matrix_mul/matrix_mul_temp.cu
@@ -32,6 +32,27 @@ void matrix_mul_cpu(float *A, float *B, float *C, int m, int k, int n){
         }
     }
 }
+
+void compare(float *hostC, float *serialC, int M, int N)
+{
+    float error = 0;
+    bool tmp = true;
+    for (int i = 0; i < M * N; i++)
+    {
+        error = fmax(error, fabs(hostC[i] - serialC[i]));
+        if (error > 1e-5)
+        {
+            tmp = false;
+            printf("error:hostC[%d] = %.3f, serialC[%d] = %.3f\n", i, hostC[i], i, serialC[i]);
+            break;
+        }
+    }
+    if (tmp)
+    {
+        printf("GPU output all right\n");
+    }
+}
+
 // GPU V1
 __global__ void matrix_mul_01(float *A, float *B, float *C, int m, int k, int n){
     int row = threadIdx.x + blockDim.x * blockIdx.x;
@@ -45,7 +66,7 @@ __global__ void matrix_mul_01(float *A, float *B, float *C, int m, int k, int n)
     }
 }
 
-// GPU V2
+// GPU V2 --使用共享内存和分段方法
 template <int BLOCK_DIM>
 __global__ void matrix_mul_02(float *dA, float *dB, float *dC, int m, int k, int n){
     int row = blockIdx.x * blockDim.x + threadIdx.x;
@@ -80,9 +101,153 @@ __global__ void matrix_mul_02(float *dA, float *dB, float *dC, int m, int k, int
     }
 }
 
-// GPU V3
-template <int BLOCK_DIM>
+// GPU V3 --一个线程处理多个元素
+template <int BM, int BN, int BK, int TM, int TN>
 __global__ void matrix_mul_03(float *dA, float *dB, float *dC, int m, int k, int n){
-    int row = blockIdx.x * blockDim.x + threadIdx.x;
+    __shared__ float SA[BM*BK];
+    __shared__ float SB[BK*BN];
 
+    int indA = TM*(threadIdx.x+blockIdx.x*blockDim.x);
+    int indB = TN*(threadIdx.y+blockIdx.y*blockDim.y);
+
+    // 分成多少段
+    int width = (k+BK-1)/BK;
+     
+    // 初始化
+    float tmp[TM*TN] = {0.0f};
+
+    for(int ph=0;ph<width;ph++){
+        // 加载矩阵到共享内存
+        for(int index_q = 0 ; index_q<TM ; index_q++){
+            for(index_k = 0 ; index_k<BK ; index_k++){
+                if(indA+index_q<M && index_k+ph*BK<K){
+                    SA[(threadIdx.x*TM+index_q)*BK+index_k] = dA[(indA+index_q)*K+index_k+ph*BK];
+                }else{
+                    SA[(threadIdx.x*TM+index_q)*BK+index_k] = 0.0f;
+                }
+            }
+        }
+        __syncthreads();
+        for(int index_p= 0;index_p<TN;index_p++){
+            for(index_k = 0;index_k<BK;index_k++){
+                if(indB+index_p<N && index_k+ph*BK<K){
+                    SB[index_k*BN+ threadIdx.y*TN + index_p] = dB[(index_k+ph*BK)*N + indB+index_p];
+                }else{
+                    SB[index_k*BN+ threadIdx.y*TN + index_p] = 0.0f;
+                }
+            }
+        }
+        __syncthreads();
+        // 计算
+        for(int index_q=0;index_q<TM;index_q++){
+            for(int index_p=0;index_p<TN;index_p++){
+                for(int index_k=0;index_k<BK;index_k++){
+                    tmp[index_q*TN + index_p] += SA[(threadIdx.x*TM+index_q)*BK+index_k]*SB[index_k*BN+threadIdx.y*TN+index_p];
+                }
+            }
+        }
+        __syncthreads();
+
+    }
+
+    for(int index_q=0;index_q<TM;index_q++){
+        for(int index_p=0;index_p<TN;index_p++){
+            if(indA+index_q<M && indB+index_p<N){
+                dC[(indA+index_q)*N+indB+index_p] = tmp[index_q*TN+index_p];
+            }
+        }
+    }
+}
+
+
+void hostMatrix(float *hostA, float *hostB, float *hostC, int M, int K, int N)
+{
+    double st, ela;
+    st = get_walltime();
+
+    float *dA, *dB, *dC;
+    cudaMalloc((void **)&dA, M * K * sizeof(float));
+    cudaMalloc((void **)&dB, N * K * sizeof(float));
+    cudaMalloc((void **)&dC, M * N * sizeof(float));
+
+    cudaMemcpy(dA, hostA, M * K * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(dB, hostB, N * K * sizeof(float), cudaMemcpyHostToDevice);
+
+    int num_blocks_x = (M + BM - 1) / BM;
+    int num_blocks_y = (N + BN - 1) / BN;
+    dim3 block_dim(BLOCK_DIM_x, BLOCK_DIM_y, 1);
+    dim3 grid_dim(num_blocks_x, num_blocks_y, 1);
+    int repeat = 20;
+    // matrixKernel1st<BM, BN, BK, TM, TN><<<grid_dim, block_dim>>>(dA, dB, dC, M, K, N);
+    // matrixKernel2nd<BM, BN, BK, TM, TN><<<grid_dim, block_dim>>>(dA, dB, dC, M, K, N);
+    // matrixOrigin<BM, BN, BK, TM, TN><<<grid_dim, block_dim>>>(dA, dB, dC, M, K, N);
+    cudaEvent_t start, stop;
+    float ker_time = 0;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start, 0);
+    for (int i = 0; i < repeat; i++)
+    {
+        // matrixKernel1st<BM, BN, BK, TM, TN><<<grid_dim, block_dim>>>(dA, dB, dC, M, K, N);
+        // matrixKernel2nd<BM, BN, BK, TM, TN><<<grid_dim, block_dim>>>(dA, dB, dC, M, K, N);
+        // matrixOrigin<BM, BN, BK, TM, TN><<<grid_dim, block_dim>>>(dA, dB, dC, M, K, N);
+    }
+
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess)
+    {
+        printf("CUDA Error: %s\n", cudaGetErrorString(err));
+        // Possibly: exit(-1) if program cannot continue....
+    }
+
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&ker_time, start, stop); // must float ker_time
+
+    cudaMemcpy(hostC, dC, M * N * sizeof(float), cudaMemcpyDeviceToHost);
+
+    cudaFree(dA);
+    cudaFree(dB);
+    cudaFree(dC);
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+    ela = get_walltime() - st;
+    printf("M-K-N: %d-%d-%d\n", M, K, N);
+    printf("GPU use time: %.4f second\n", ela);
+    printf("kernel time: %.4f second, %.4f ms\n", ker_time / (repeat * 1000.), ker_time / repeat);
+    printf("grid dim: %d, %d, %d\n", grid_dim.x, grid_dim.y, grid_dim.z);
+    printf("block dim: %d, %d, %d\n", block_dim.x, block_dim.y, block_dim.z);
+}
+
+int main()
+{
+    float *hostA, *hostB, *hostC, *serialC;
+    int M = 1024;
+    int K = 1024;
+    int N = 1024;
+
+    hostA = (float *)malloc(M * K * sizeof(float));
+    hostB = (float *)malloc(N * K * sizeof(float));
+    hostC = (float *)malloc(M * N * sizeof(float));
+    serialC = (float *)malloc(M * N * sizeof(float));
+    for (int i = 0; i < M * K; i++)
+    {
+        hostA[i] = i % 3;
+    }
+    for (int i = 0; i < N * K; i++)
+    {
+        hostB[i] = i % 3;
+    }
+    hostMatrix(hostA, hostB, hostC, M, K, N);
+    double st, ela;
+    st = get_walltime();
+    matrixSerial(hostA, hostB, serialC, M, K, N);
+    ela = get_walltime() - st;
+    printf("CPU time:%.2f second\n", ela);
+    compare(hostC, serialC, M, N);
+    free(hostA);
+    free(hostB);
+    free(hostC);
+    free(serialC);
+    return 0;
 }
